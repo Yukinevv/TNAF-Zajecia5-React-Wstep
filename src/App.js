4ca504be67a79ba9941ccbc58602f04b2dd309ca
Yukinevv <adrianrodzic33@gmail.com>
2023-01-10 11:08:45 +0100

    Zadanie3 Info o uzytkowniku

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App(props) {
+
+  const users = [{
+    "name": "John Doe",
+    "dateBirth": "1970-01-01",
+    "email": "john@example.com"
+  },
+  {
+    "name": "Jane Doe",
+    "dateBirth": "1975-01-01",
+    "email": "jane@example.com"
+  },
+  {
+    "name": "Bob Smith",
+    "dateBirth": "1989-01-01",
+    "email": "bob@example.com"
+  }];
+
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(JSON.parse(e.target.value));
+  };
+
   return (
-    <h1>Hello {props.name}!</h1>
+    <div id="container">
+      <h1>User Information</h1>
+
+      <select id="userChoose" onChange={handleChange}>
+        <option selected disabled hidden>Select a user</option>
+        {users.map(element => <option value={JSON.stringify(element)} key={element.name}>{element.name}</option>)}
+      </select>
+
+      <h2>{value.name}</h2>
+      <p>{value.dateBirth} <br />
+        {value.email}</p>
+
+    </div>
   );
 }
 
